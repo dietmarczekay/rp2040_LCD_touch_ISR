9bed687b89e58e598b60c67666125ad966a2920b
Dietmar Czekay <dietmar@czekay.org>
2023-02-16 08:17:04 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,50 +15,10 @@ TFT_eSPI tft = TFT_eSPI();
 
 XPT2046_Touchscreen ts(TOUCH_CS, TFT_IRQ);
 
-/*  Adafruit_ST7796S_kbv(int8_t _CS, int8_t _DC, int8_t _MOSI, int8_t _SCLK,
-                   int8_t _RST = -1, int8_t _MISO = -1);*/
-//Adafruit_ST7796S_kbv tft = Adafruit_ST7796S_kbv(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST, TFT_MISO);
-
 bool toggle = false;
 bool toggle2 = false;
 uint32_t atime = 0;
 uint16_t debounce = 600;
-/*
-// Code to run a screen calibration, not needed when calibration values set in setup()
-void touch_calibrate()
-{
-  uint16_t calData[6];
-
-  // Calibrate
- 
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(20, 0);
-  tft.setTextFont(2);
-  tft.setTextSize(1);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-
-  tft.println("Touch corners as indicated");
-
-  tft.setTextFont(1);
-  tft.println();
-
-  tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
-
-
-  EEPROM.begin(sizeof(calData));
-  EEPROM.put(EEPROM_ADDR, calData);  
-  EEPROM.commit();
-  EEPROM.end(); 
- 
-  tft.fillScreen(TFT_BLACK);
-  
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.println("Calibration complete!");
-  tft.println("Calibration code sent to Serial port.");
-
-  delay(3000);
-}
-*/
 
 void callInterrupt(uint gpio, uint32_t event)
 {
@@ -99,23 +59,13 @@ void setup()
   }
 
   tft.init();
-  //tft.begin();
-  //tft.fillScreen(ST7796S_BLACK);
 
   tft.fillScreen(TFT_BLACK);
 
   tft.println("test");
   ts.begin();
-  //tft.println("test");
 
   Serial.println("Setup done22");
-
-  //pinMode(TFT_RST, INPUT_PULLUP);
-  /* gpio_init(TFT_IRQ);
-  gpio_set_dir(TFT_IRQ, GPIO_IN);
-  gpio_pull_up(TFT_IRQ);
-  gpio_set_irq_enabled_with_callback(TFT_IRQ, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &callInterrupt);
-     */
   
   SPI.usingInterrupt(digitalPinToInterrupt(TFT_IRQ));
   attachInterrupt(digitalPinToInterrupt(TFT_IRQ), callInterrupt2, LOW);
@@ -128,9 +78,5 @@ void loop()
   toggle = !toggle;
   sleep_ms(50);
   tft.setCursor(0, 0);
-  //tft.setTextColor(ST7796S_WHITE);  tft.setTextSize(1);
   tft.println("Hello World!");
-  //if (ts.touched()) {
-    //Serial.println("meine");
-  //}
 }
